f2998e472cb484a06f053c6e08dd332ca1b821cf
STOL7 <prijenkhokhani58@gmail.com>
2021-05-30 14:30:35 +0530

    More changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    HomeComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
